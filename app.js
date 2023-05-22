a891d0107dc0920d95332f65198e7052782c3df6
ritesh-25 <100701018+ritesh-25@users.noreply.github.com>
2023-05-22 17:35:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,54 @@ res.sendFile(__dirname+ "/Login-creator.html");
 app.get("/logincompany",function(req,res){
 res.sendFile(__dirname + "/Login-creator.html");
 });
+mongoose.connect('mongodb+srv://riteshbaindara25:Ai9L6V2WquLRNmug@cluster0.stgntt6.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+})
+
+const creatorSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+});
+
+const sec = "Thisisourliitlesecret";
+creatorSchema.plugin(encrypt,{secret:sec, encryptedFields:["password"]});
+const creator = mongoose.model("creator",creatorSchema);
+app.post("/creatorsignup",function(req,res){
+const newcreator = new creator({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+});
+newcreator.save();
+res.write("<h1>ho gya tu signup bhadwe</h1>");
+res.send;
+});
+
+app.post("/creatorlogin",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    // console.log(username);
+    creator.findOne({username:username}).then(post=>{
+        if(post.password===password){
+          res.write("<h1>successfully login</h1>");
+          res.send;
+        }
+        else{
+            console.log(post.password);
+        }
+        });
+});
+
+
+
 
 app.listen(3000,function(){
     console.log("haa bhai kam  gya");
 });
+// Ai9L6V2WquLRNmug
